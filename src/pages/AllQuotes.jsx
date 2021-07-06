53ac10d95f7cba94def9628907647b3006ec7c93
jamalashrafi <jamalnabi07@gmail.com>
2021-07-06 17:20:27 +0530

    add: added sorting logic to the all Quotes list.

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,37 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getQuoteList } from '../actions/quoteAction';
 import { NavLink } from 'react-router-dom';
 
 const AllQuotes = () => {
-
+    const [toggleBtn, setToggleBtn] = useState('Sort Ascending');
+    const [quoteListState, setQuoteListState] = useState([]);
     const quoteList = useSelector(state => state.quoteReducer.quotes);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getQuoteList());
-    })
+        setQuoteListState(quoteList);
+    }, [quoteList, dispatch]);
 
-    if (quoteList.length === 0)
+    const sortAr = (order) => {
+        let quoteArList = [...quoteListState];
+        if (order === 'descending') {
+            quoteArList.sort((a, b) => {
+                if (a.quote > b.quote)
+                    return -1;
+                if (a.quote < b.quote)
+                    return 1;
+                return 0;
+            });
+        } else {
+            quoteArList.sort((a, b) => {
+                if (a.quote < b.quote)
+                    return -1;
+                if (a.quote > b.quote)
+                    return 1;
+                return 0;
+            });
+        }
+        setQuoteListState(quoteArList);
+    }
+
+    const handleToggle = () => {
+        if (toggleBtn === 'Sort Ascending') {
+            setToggleBtn('Sort Descending');
+            sortAr('ascending')
+        }
+        else if (toggleBtn === 'Sort Descending') {
+            setToggleBtn('Sort Ascending');
+            sortAr('descending')
+        }
+
+    }
+
+    if (quoteListState.length === 0)
         return 'Loading...'
     return (
-        <div>
-            {
-                quoteList.map((quote, index) => (
-                    <div key={index}>
-                        <span >{quote.quote}</span>
-                        <NavLink to={{
-                            pathname: `/all-quotes/${quote.quote}`,
-                            aboutProps: {
-                                userName: quote.userName,
-                                quoteText: quote.quote
-                            }
-                        }}>View FullScreen</NavLink>
-                    </div>)
-                )
-            }
-        </div>
+        <>
+            <button onClick={handleToggle}>{toggleBtn}</button>
+            <div>
+                {
+                    quoteListState.map((quote, index) => (
+                        <div key={index}>
+                            <span >{quote.quote}</span>
+                            <NavLink to={{
+                                pathname: `/all-quotes/${quote.quote}`,
+                                aboutProps: {
+                                    userName: quote.userName,
+                                    quoteText: quote.quote
+                                }
+                            }}>View FullScreen</NavLink>
+                        </div>)
+                    )
+                }
+            </div>
+        </>
     )
 }
 
