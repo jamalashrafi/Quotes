c6780f4c270491cf95745273d7a9006ef3639cdf
jamalashrafi <jamalnabi07@gmail.com>
2021-07-06 15:03:12 +0530

    add: added addQuote and get quote reducer entries.

--- src/reducer/quoteReducer.js
+++ src/reducer/quoteReducer.js
@@ -1,5 +1,15 @@
-const quoteReducer = (state = {}, action) => {
-    switch (action.payload) {
+import { ADD_QUOTE, GET_QUOTES } from '../constants'
+const INITIAL_STATE = {
+    quotes: []
+}
+
+
+const quoteReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case ADD_QUOTE:
+            return { ...state, quotes: [...state.quotes, action.payload] }
+        case GET_QUOTES:
+            return { quotes: state.quotes } //Sending only the required part of the state not the compelete state.
         default:
             return state
     }
