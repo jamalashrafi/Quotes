d8b9f4a0a258990bf8c6f386d886ea4f953c2a76
jamalashrafi <jamalnabi07@gmail.com>
2021-07-06 15:02:08 +0530

    add: added Welcome.jsx

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Welcome = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Welcome
