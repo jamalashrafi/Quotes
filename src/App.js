8f087bcb28a825a25560c7a9358993e6c7ca8fa6
jamalashrafi <jamalnabi07@gmail.com>
2021-07-06 14:59:55 +0530

    add: added routes for add quotes and list all quotes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import { Switch, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import AddQuote from './pages/AddQuote';
+import AllQuotes from './pages/AllQuotes';
 function App() {
   return (
     <div className="App">
-      My App
+
+      <Route path='/' component={Welcome} />
+      <Route path='/add-a-quote' exact component={AddQuote} />
+      <Route path='/all-quotes' exact component={AllQuotes} />
+
     </div>
   );
 }
