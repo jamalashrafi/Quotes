1222c8ec47aa4be1f6df2202b2642a26420ece11
jamalashrafi <jamalnabi07@gmail.com>
2021-07-07 08:08:54 +0530

    add: added alternate way of rendering header using Layout component. add: added Redirect when url is at root(/) level.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import { Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
+import { Route, Switch, Redirect } from 'react-router-dom';
+// import Welcome from './pages/Welcome';
 import AddQuote from './pages/AddQuote';
 import AllQuotes from './pages/AllQuotes';
 import SpecificQuote from './pages/SpecificQuote';
+import Layout from './components/Layout';
+// function App() {
+//   return (
+//     <div className="App">
+
+//       <Route path='/' component={Welcome} />
+//       <Route path='/add-a-quote' exact component={AddQuote} />
+//       <Route path='/all-quotes' exact component={AllQuotes} />
+//       <Route path='/all-quotes/:quote' component={SpecificQuote} />
+
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div className="App">
-
-      <Route path='/' component={Welcome} />
-      <Route path='/add-a-quote' exact component={AddQuote} />
-      <Route path='/all-quotes' exact component={AllQuotes} />
-      <Route path='/all-quotes/:quote' component={SpecificQuote} />
-
-    </div>
-  );
+    <Layout>
+      <Switch>
+        <Route path='/' exact >
+          <Redirect to='/all-quotes' />
+        </Route>
+        <Route path='/add-a-quote' exact component={AddQuote} />
+        <Route path='/all-quotes' exact component={AllQuotes} />
+        <Route path='/all-quotes/:quote' component={SpecificQuote} />
+      </Switch>
+    </Layout>
+  )
 }
 
 export default App;
